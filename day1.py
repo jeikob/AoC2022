97508a84b7a01e030376d99b5c1b21f6e51c3087
jeikob <rexro98@gmail.com>
2022-12-02 09:41:02 -0600

    Day 1 more concise :)

--- day1.py
+++ day1.py
@@ -1,7 +1,4 @@
-file = open("day1.txt")
-inputStr = file.read()
-
-elves = [sum([eval(i) for i in arr.split('\n')]) for arr in inputStr.split('\n\n')]
+elves = [sum([eval(i) for i in arr.split('\n')]) for arr in open("day1.txt").read().split('\n\n')]
 elves.sort(reverse=True)
 
 print(max(elves))
